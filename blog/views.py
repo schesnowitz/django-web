35a3240f3593e13bbcde832c263875b0ac8a3cfb
Stephen Chesnowitz <steve@chesnowitz.com>
2023-02-01 23:38:22 -0500

    post model

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,28 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from.models import Post
 
-posts = [
-    {
-        'author' : 'Hank Smith',
-        'title' : "Post 1",
-        'content' : 'post 1 content',
-        'date_posted ': 'August 1, 2025',  
-    },
-    {
-        'author' : 'Steve o',
-        'title' : "Post 2",
-        'content' : 'post 2 content content really coming out of this file',
-        'date_posted ': 'August 2, 2025',  
-    }
-]
+# posts = [
+#     {
+#         'author' : 'Hank Smith',
+#         'title' : "Post 1",
+#         'content' : 'post 1 content',
+#         'date_posted ': 'August 1, 2025',  
+#     },
+#     {
+#         'author' : 'Steve o',
+#         'title' : "Post 2",
+#         'content' : 'post 2 content content really coming out of this file',
+#         'date_posted ': 'August 2, 2025',  
+#     }
+# ]
 
 
 
 def home(request):
 
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context) 
 
