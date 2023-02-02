7e211f2e57f6ff06568d50aaf8cd6fb9610e5980
Stephen Chesnowitz <steve@chesnowitz.com>
2023-02-01 20:03:11 -0500

    basic render routes with some fake data

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+posts = [
+    {
+        'author' : 'Hank Smith',
+        'title' : "Post 1",
+        'content' : 'post 1 content',
+        'date_posted ': 'August 1, 2025',  
+    },
+    {
+        'author' : 'Steve o',
+        'title' : "Post 2",
+        'content' : 'post 2 content content really coming out of this file',
+        'date_posted ': 'August 2, 2025',  
+    }
+]
+
+
 
 def home(request):
-    return HttpResponse("<h1>Home Page</h1>") 
+
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context) 
 
 def about(request):
-    return HttpResponse("<h1>About Page</h1>") 
+    return render(request, 'blog/about.html') 
 
