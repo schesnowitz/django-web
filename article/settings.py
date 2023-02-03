361cf3ce8de59c2ebe306f25edc1bc5017f3c932
Stephen Chesnowitz <steve@chesnowitz.com>
2023-02-03 01:13:06 -0500

    users 2 db and crispy forms

--- article/settings.py
+++ article/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
+    "crispy_forms",
+    "crispy_bootstrap5",
     'blog',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -118,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
