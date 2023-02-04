d4935b1d05f4126b89731e83fc3755ff7894b773
Stephen Chesnowitz <steve@chesnowitz.com>
2023-02-04 15:50:42 -0500

    crud posts with views

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,20 @@
 from .import views
+from .views import (
+    PostListView, 
+    PostDetailView, 
+    PostCreateView, 
+    PostUpdateView,
+    PostDeleteView
+    )
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    # path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    # PostCreateView is shared with update and the convention is to have the view as post_form.html
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'), 
     path('about/', views.about, name='blog-about'),
 ] 
--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,70 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+# for class based views... 
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView, 
+    UpdateView, 
+    DeleteView
+    )
+from django.urls import reverse_lazy
 from.models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-# posts = [
-#     {
-#         'author' : 'Hank Smith',
-#         'title' : "Post 1",
-#         'content' : 'post 1 content',
-#         'date_posted ': 'August 1, 2025',  
-#     },
-#     {
-#         'author' : 'Steve o',
-#         'title' : "Post 2",
-#         'content' : 'post 2 content content really coming out of this file',
-#         'date_posted ': 'August 2, 2025',  
+
+# def home(request):
+
+#     context = {
+#         'posts' : Post.objects.all()
 #     }
-# ]
+#     return render(request, 'blog/home.html', context) 
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # TemplateDoesNotExist at / blog/post_list.html
+    context_object_name = 'posts' # setting name for view as 
+    # otherwise looks for the default which is object list
+    ordering = ['-date_posted'] # (variable) ordering: list[str]
 
+class PostDetailView(DetailView):
+    model = Post
 
-def home(request):
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
 
-    context = {
-        'posts' : Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context) 
+    def form_valid(self, form):
+        form.instance.author = self.request.user 
+        return super().form_valid(form)
+    # success_url = reverse_lazy('blog-home')
+''' post created but error -> No URL to redirect to.  
+Either provide a url or define a get_absolute_url method on the Model. 
+this is set in the model get_absolute_url to redirect to home, for example use success url'''
+class PostUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user 
+        return super().form_valid(form)
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False 
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
     return render(request, 'blog/about.html') 
+
+
 
